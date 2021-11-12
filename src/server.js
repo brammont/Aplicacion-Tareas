826ac5b9466856cfbedf15ae3f521c439e81e83e
Brammont <brayanmazuera1@gmail.com>
2021-11-12 16:37:34 -0500

    configurnado el modulo morgan para ver la respuesta del navegador y lo que pide el cliente

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
 
-//rutas
 
 //configuracion
 app.set('port',process.env.PORT || 3000);
 app.set('views', path.join(__dirname,'views'));
-// middlewares
+app.set('view engine','ejs');
 
+// middlewares
+app.use(morgan('dev'))
+//rutas
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
