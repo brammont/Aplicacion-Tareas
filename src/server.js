971b34c9444a0ec169b71986343836229bab7372
Brammont <brayanmazuera1@gmail.com>
2021-11-12 16:43:17 -0500

    configurnado el enrutador princiapl y express para que acepte datos desde el html

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
-
+const indexRoutes = require('./routes/index')
 
 //configuracion
 app.set('port',process.env.PORT || 3000);
@@ -11,8 +10,11 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 // middlewares
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.urlencoded);
 //rutas
+app.use('/',indexRoutes);
+//start server
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
