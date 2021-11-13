8ce8a9b27f6196803f91dcef50083c95fefeffda
Brammont <brayanmazuera1@gmail.com>
2021-11-12 23:08:25 -0500

    mostrando los datos en patan de forma rudimentaria

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Task = require('../models/task')
-router.get('/',(req,res)=>{
-    res.render('index');
+router.get('/',async (req,res)=>{
+    const tasks = await Task.find();
+    console.log(tasks)
+    res.render('index',{
+        tasks //tasks:tasks
+    });
 });
-router.post('/add',(req,res)=>{
-    console.log(new Task(req.body))
-    res.send("recived")
+router.post('/add', async (req,res)=>{
+    const task = new Task(req.body)
+    await task.save();
+    res.send("recived");
 });
 module.exports = router;
