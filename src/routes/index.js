ff5a01557482bb382298e58b1eb704de9becf636
Brammont <brayanmazuera1@gmail.com>
2021-11-12 22:57:49 -0500

    agregando una nueva tarea en DB

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const Task = require('../models/task')
 router.get('/',(req,res)=>{
     res.render('index');
 });
 router.post('/add',(req,res)=>{
-    console.log(req.body)
+    console.log(new Task(req.body))
     res.send("recived")
 });
 module.exports = router;
