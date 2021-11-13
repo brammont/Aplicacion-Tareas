38ed663ebad4b9b33458d9bc03160ce7544ae7aa
Brammont <brayanmazuera1@gmail.com>
2021-11-12 23:54:40 -0500

    creando la ruta editar para hacer update de los datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,6 +13,18 @@ router.post('/add', async (req,res)=>{
     await task.save();
     res.redirect('/');
 });
+router.get('/convertido/:id',async (req,res)=>{
+    const {id}= req.params;
+    const task = await Task.findById({_id:id});
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
+router.get('/editar/:id',async (req,res)=>{
+    const {id}= req.params;
+    const task = await Task.findById({_id:id});
+    res.render('edit');
+});
 router.get('/delete/:id',async (req,res)=>{
     const {id}= req.params;
     await Task.remove({_id:id});
