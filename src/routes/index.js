0a275eca678bcf79241d129585c646ac219c819f
Brammont <brayanmazuera1@gmail.com>
2021-11-12 23:36:04 -0500

    creando la ruta para borrar

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,12 @@ router.get('/',async (req,res)=>{
 router.post('/add', async (req,res)=>{
     const task = new Task(req.body)
     await task.save();
-    res.redirect('/')
+    res.redirect('/');
 });
+router.get('/delete/:id',async (req,res)=>{
+    const {id}= req.params;
+    await Task.remove({_id:id});
+    res.redirect('/');
+});
+
 module.exports = router;
